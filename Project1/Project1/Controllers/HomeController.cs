c2ed404799d91ec4414ce681c7988de41eea2c97
Your Name <you@example.com>
2019-03-10 23:04:53 -0500

    Added a home page

--- Project1/Project1/Controllers/HomeController.cs
+++ Project1/Project1/Controllers/HomeController.cs
@@ -4,21 +4,80 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Project1.BLL.IDataRepos;
 using Project1.Models;
+using MoreLinq;
 
 namespace Project1.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ICustomerRepo customerRepo, ILocationRepo locationRepo,
+            IOrderRepo orderRepo, ICupcakeRepo cupcakeRepo, IOrderItemRepo orderItemRepo,
+            ILocationInventoryRepo locationInventoryRepo, IRecipeItemRepo recipeItemRepo,
+            ILogger<HomeController> logger)
         {
-            return View();
+            LocRepo = locationRepo;
+            CustomerRepo = customerRepo;
+            OrderRepo = orderRepo;
+            CupcakeRepo = cupcakeRepo;
+            OrderItemRepo = orderItemRepo;
+            LocationInventoryRepo = locationInventoryRepo;
+            RecipeItemRepo = recipeItemRepo;
+
+            _logger = logger;
         }
 
-        public IActionResult Privacy()
+        public ILocationRepo LocRepo { get; set; }
+        public ICustomerRepo CustomerRepo { get; set; }
+        public IOrderRepo OrderRepo { get; set; }
+        public ICupcakeRepo CupcakeRepo { get; set; }
+        public IOrderItemRepo OrderItemRepo { get; set; }
+        public ILocationInventoryRepo LocationInventoryRepo { get; set; }
+        public IRecipeItemRepo RecipeItemRepo { get; set; }
+
+        public IActionResult Index()
         {
-            return View();
+            Project1.ViewModels.HomeViewModel viewModel = new ViewModels.HomeViewModel
+            {
+                Locations = LocRepo.GetAllLocations().ToList(),
+                Customers = CustomerRepo.GetAllCustomers().ToList(),
+                Cupcakes = CupcakeRepo.GetAllCupcakes().ToList(),
+                Orders = OrderRepo.GetAllOrders().ToList(),
+                OrderItems = OrderItemRepo.GetAllOrderItems().ToList()
+
+            };
+            decimal sum = 0;
+            decimal incrementer = 0;
+
+            foreach (var order in viewModel.Orders)
+            {
+                sum += order.GetTotalCost(OrderItemRepo.GetOrderItems(order.Id).ToList(), viewModel.Cupcakes);
+                incrementer++;
+            }
+            if (incrementer > 0)
+            {
+                viewModel.OrderTotalAverage = sum / incrementer;
+            }
+
+            if (viewModel.Locations != null)
+            {
+                viewModel.LocationMostOrders = viewModel.Locations.MaxBy(sL =>
+                LocRepo.GetLocationOrderHistory(sL.Id).Count()).OrderBy(sL => sL.Id).First().Name;
+                viewModel.LocationWithLatestOrder = viewModel.Locations.Single(l => l.Id ==
+                    viewModel.Orders.MaxBy(o => o.OrderTime).First().OrderLocation).Name;
+            }
+
+            return View(viewModel);
         }
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
